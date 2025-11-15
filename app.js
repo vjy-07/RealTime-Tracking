789ff643af7592afe0fe16b96db5d78a553061a2
vjy-07 <gvijaycrl@gmail.com>
2025-11-15 18:41:21 +0530

    PORT updated

--- app.js
+++ app.js
@@ -64,6 +64,8 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
 });
